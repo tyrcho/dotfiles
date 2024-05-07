bd66d3a1462e7833aca6707a7b5645cdcf416893
Michel Daviot <michel.daviot@datadoghq.com>
2024-05-07 11:58:43 +0200

    basic folding

--- .config/nvim/lua/custom/core/options.lua
+++ .config/nvim/lua/custom/core/options.lua
@@ -53,3 +53,9 @@ vim.opt.colorcolumn = "120"
 
 -- for vim-gh-line, for some reason it doesn't work if I put it in the plugin config
 vim.g.gh_open_command = 'fn() { echo "$@" | pbcopy; }; fn '
+
+-- fold
+vim.opt.foldmethod = "indent"
+vim.opt.fillchars = { fold = " " }
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
