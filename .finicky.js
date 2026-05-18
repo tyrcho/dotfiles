2614f02ec7070b77f1eaf95d422e3fb9f53b2e9b
Michel Daviot <michel.daviot@datadoghq.com>
2026-05-18 15:35:33 +0200

    feat(finicky): route Google Workspace links to work profile
    Add docs.google.com / drive / sheets / slides / mail / gmail and
    Microsoft (login.microsoftonline.com, portal.azure.com) to the
    Datadog profile. Previously these hostnames were defined but never
    referenced, so links fell through to defaultBrowser with no profile,
    opening in whichever Chrome profile was focused (including the
    test-automation profile).
    
    Drop the unused govcloud and ninja configuration:
    - ChromeProfiles.DatadogGov, DatadogNinja (profiles do not exist
      locally)
    - GoogleIdp.DdGov, DdGovVault, DdNinja
    - AWSFedDirectoryId (only staging-fed was referenced, and its
      handler is gone)
    - start.us-gov-home.awsapps.com and app.ddog-gov.com handlers
    - Collapse the 4-way IDP ternary to Datadog-vs-Personal
    
    Also drop salesforce (no longer used) and switch from legacy
    module.exports to modern ESM export default per Finicky's
    deprecation warning. Convert top-level identifiers to const and
    use profile names ("Datadog", "Michel") instead of paths
    ("Default", "Profile 1") per Finicky v4 guidance.

---
+++ .finicky.js
@@ -0,0 +1,95 @@
+const googleWorkspaceHostnames = [
+    "gmail.com",
+    "mail.google.com",
+    "drive.google.com",
+    "docs.google.com",
+    "sheets.google.com",
+    "slides.google.com",
+    "docs.new",
+    "sheets.new",
+    "slides.new",
+    "forms.new",
+]
+
+const microsoftHostnames = [
+    "login.microsoftonline.com",
+    "portal.azure.com",
+]
+
+
+const ChromeProfiles = {
+    Datadog: "Datadog",
+    Personal: "Michel",
+}
+
+const GoogleIdp = {
+    Dd: "C0147pk0i",
+    DdVault: "1023266953562-2nltfg1vsh570fr2npin7klkn8lo3g6e.apps.googleusercontent.com",
+};
+
+export default {
+    defaultBrowser: "Google Chrome",
+    handlers: [
+        {
+            match: finicky.matchHostnames(["accounts.google.com"]),
+            browser: ({ urlString }) => ({
+                name: "Google Chrome",
+                profile: urlString.includes(GoogleIdp.Dd) || urlString.includes(GoogleIdp.DdVault)
+                    ? ChromeProfiles.Datadog
+                    : ChromeProfiles.Personal,
+            }),
+        },
+        {
+            match: finicky.matchHostnames([
+                "github.com",
+                "cs.github.com",
+                "gist.github.com",
+                "app.datadoghq.com",
+                "ddstaging.datadoghq.com",
+                "datadoghq.atlassian.net",
+                "sdp.ddbuild.io",
+                "datadog.zoom.us",
+                "dd.datad0g.com",
+                "gitlab.ddbuild.io",
+                "mosaic.us1.ddbuild.io",
+                "dd.slack.com",
+                "dd.enterprise.slack.com",
+                "datadog.pagerduty.com",
+                "cnap-api.sdm.ddbuild.io",
+                "cnab-api-sdm1.ddbuild.io",
+                "signin.aws.amazon.com",
+            ]),
+            browser: {
+                name: "Google Chrome",
+                profile: ChromeProfiles.Datadog,
+            },
+        },
+        {
+            match: finicky.matchHostnames([
+                ...googleWorkspaceHostnames,
+                ...microsoftHostnames,
+            ]),
+            browser: {
+                name: "Google Chrome",
+                profile: ChromeProfiles.Datadog,
+            },
+        },
+        {
+            match: finicky.matchHostnames([
+                "facebook.com",
+                "youtube.com",
+                "x.com",
+                "mtg.wiki",
+            ]),
+            browser: {
+                name: "Google Chrome",
+                profile: ChromeProfiles.Personal,
+            },
+        },
+        {
+            match: /zoom\.us\/j/,
+            browser: "us.zoom.xos"
+        },
+    ],
+};
+
